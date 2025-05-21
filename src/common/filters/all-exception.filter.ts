05931f67b88e1b4739d8971f0aed80a2d064d2ba
jpon0113 <jpongama@gamil.com>
2025-05-21 12:53:31 +0800

    feat: 1. .env .env.development新增參數 2. 安裝 request-ip 3. 新增全局錯誤攔截器 all-exception.filter.ts 4. main.ts 引入參數判斷: cors, errorFilterFlag等

---
+++ src/common/filters/all-exception.filter.ts
@@ -0,0 +1,55 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+
+import * as requestIp from 'request-ip';
+
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger();
+
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+    const response = ctx.getResponse();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const msg: unknown = exception['response'] || 'Internal Server Error';
+
+    // 加入更多異常錯誤判斷邏輯
+    // if (exception instanceof QueryFailedError) {
+    //   msg = exception.message;
+    //   // if (exception.driverError.errno && exception.driverError.errno === 1062) {
+    //   //   msg = '';
+    //   // }
+    // }
+
+    const responseBody = {
+      headers: request.headers,
+      query: request.query,
+      body: request.body,
+      params: request.params,
+      timestamp: new Date().toISOString(),
+      // IP資訊
+      ip: requestIp.getClientIp(request),
+      exceptioin: exception['name'],
+      error: msg,
+    };
+
+    this.logger.error('[JPON]', responseBody);
+    httpAdapter.reply(response, responseBody, httpStatus);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { AllExceptionFilter } from './common/filters/all-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // 新增nest-winston
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  // 取得設定檔
   const configServcie = app.get(ConfigService);
   const port = configServcie.get<number>('PORT', 3000);
+  const cors = configServcie.get('CORS', false);
+  const prefix = configServcie.get('PREFIX', '/api');
+  const version = configServcie.get<string>('VERSION', '1');
+  const errorFilterFlag = configServcie.get<string>('ERROR_FILTER');
+  // 新增nest-winston
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
+  app.setGlobalPrefix(prefix);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: [version],
+  });
+  // 允許跨域
+  if (cors === 'true') {
+    app.enableCors();
+  }
+  // 允許錯誤攔截
+  if (errorFilterFlag === 'true') {
+    const httpAdapter = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+  }
+
   await app.listen(port);
 }
 bootstrap();
